dd6ab8d24bb27caf20eed9b90921a13410783948
lsa77 <lukasz.sawicki.2@gmail.com>
2018-01-27 20:16:30 +0100

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef vector<int> Vec;
+typedef vector<Vec> Mat;
+
+
+Mat operator*(const Mat &a, const Mat &b){
+    int i,j, tmp;
+  
+    int m = a.size();
+    int n = b.size();
+    
+    if (m<>n) return -1;
+
+    Mat prod(m);
+
+  for(i = 0; i < m; i++)
+      tmp=0;
+      for (j=0; j < m ; j++) {
+        prod[i][j] += a[i][j] * b[j][i];                  
+      }
+  }
+  return prod;
+}
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n=0;
+    int tmp;
+    Mat A;
+    
+    cin >> n;
+    for (int i=0;i<n;i++)
+        for (int j=0;j<n;j++) {
+            cin >> tmp;
+            a[i][j] = tmp;        
+        }
+    return 0;
+}
